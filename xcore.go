c8e8c52be9f89a27e79db5fb88ee56356a0736ad
metalwolf <phil@thomassigny.org>
2019-11-05 10:34:26 -0600

    patch v0.1.1

--- xcore.go
+++ xcore.go
@@ -8,7 +8,7 @@
 		- XTemplate: template system with meta language.
 
 	The Package hast been used for years on professional PHP projects in the WebAbility Core for PHP program.
-*/
+
 // XCache is a library to cache all the data you want into current application memory for a very fast access to the data.
 // The access to the data support multithreading and concurrency. For the same reason, this type of cache is not persistant (if you exit the application)
 // and cannot grow too much (as memory is the limit).
@@ -46,7 +46,45 @@
 //   data, invalid := myfiles.Get("https://developers.webability.info:82/");
 //   
 // }
-
+//
+// The XLanguage table of text entries can be loaded from XML file, XML string or normal file or string.
+// XML Format is:
+// <?xml version="1.0" encoding="UTF-8"?>
+// <language id="NAMEOFLANGUAGE" lang="LG">
+//   <entry id="ENTRYNAME">ENTRYVALUE</entry>
+//   <entry id="ENTRYNAME">ENTRYVALUE</entry>
+// </language>
+// where NAMEOFLANGUAGE is the name of your table entry, for example "homepate", "user_report", etc
+//       LG is the ISO-3369 2 letters language ID, for example "es" for spanish, "en" for english
+//       ENTRYNAME is the ID of the entry, for example "greating", "yourname", "submitbutton"
+//       ENTRYVALUE is the text for your entry, for example "Hello", "You are:", "Save" if your table is in english
+// 
+// Flat Text format is:
+// ENTRYNAME=ENTRYVALUE
+// ENTRYNAME=ENTRYVALUE
+// where ENTRYNAME is the ID of the entry, for example "greating", "yourname", "submitbutton"
+//       ENTRYVALUE is the text for your entry, for example "Hello", "You are:", "Save" if your table is in english
+// 
+// There is no name of table or language in this format (you "know" what you are loading)
+// 
+// The advantage to use XML forma is to have more control over your language, and eventyally add attributes into your entry,
+// for instance translated="yes/no", verified="yes/no", and any other data that your system could insert
+// 
+// Create a new XLanguage empty structure:
+// 
+// - NewXLanguage
+// 
+// There are 4 functions to create the language from a file or string, flat text or XML text:
+// 
+// - NewXLanguageFromXMLFile
+// - NewXLanguageFromXMLString
+// - NewXLanguageFromFile
+// - NewXLanguageFromString
+// 
+// Then you can use the set of basic access functions:
+// 
+// - Set/Get/Del/SetName/SetLanguage/GetName/GetLanguage
+*/
 package xcore
 
 // VERSION: is the used version nombre of the XCore library.
