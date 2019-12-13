709b3821de8910e45c4e1c43ef5c5f20553c63f4
metalwolf <phil@thomassigny.org>
2019-12-13 11:42:29 -0600

    patch v0.2.1

--- xcache_test.go
+++ xcache_test.go
@@ -1,19 +1,24 @@
 package xcore
 
-import "testing"
-
 import (
 	"fmt"
+	"testing"
+	"time"
 )
 
-func ExampleXCache() {
-	cache := NewXCache("cacheid", 0, 0)
+func ExampleNewXCache() {
+	cache1 := NewXCache("cacheid1", 0, 0)
+	cache2 := NewXCache("cacheid2", 100, 0)
+	cache3 := NewXCache("cacheid3", 0, 100*time.Minute)
 
-	fmt.Println(cache.Count())
+	fmt.Println("All caches empty:", cache1.Count(), cache2.Count(), cache3.Count())
+	// Output: All caches empty: 0 0 0
 }
 
 func TestXCache(t *testing.T) {
-	cache := NewXCache("cacheid", 0, 0)
+	cache1 := NewXCache("cacheid1", 0, 0)
+	cache2 := NewXCache("cacheid2", 100, 0)
+	cache3 := NewXCache("cacheid3", 0, 100*time.Minute)
 
-	fmt.Println(cache.Count())
+	fmt.Println("All caches empty: ", cache1.Count(), cache2.Count(), cache3.Count())
 }
