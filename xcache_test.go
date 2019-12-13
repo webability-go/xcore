52f5e119538447b671a48dc47a3ed364bc575429
metalwolf <phil@thomassigny.org>
2019-12-13 11:22:26 -0600

    patch v0.2.1

---
+++ xcache_test.go
@@ -0,0 +1,19 @@
+package xcore
+
+import "testing"
+
+import (
+  "fmt"
+)
+ 
+func ExampleXCache() {
+  cache := NewXCache("cacheid", 0, 0)
+
+  fmt.Println(cache.Count())
+}
+
+func TestXCache(t *testing.T) {
+  cache := NewXCache("cacheid", 0, 0)
+
+  fmt.Println(cache.Count())
+}
